fee4042846d821cf7c44d43ecb88956569b69c6e
Gasper <gasper.rebernak@icloud.com>
2015-06-11 12:29:34 +0200

    Simple demo tableView for listing categories

--- MoltinSDK iOS Example/MoltinSDK iOS Example/ViewController.h
+++ MoltinSDK iOS Example/MoltinSDK iOS Example/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
--- MoltinSDK iOS Example/MoltinSDK iOS Example/ViewController.m
+++ MoltinSDK iOS Example/MoltinSDK iOS Example/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "Moltin.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSArray *categories;
 
 @end
 
@@ -16,12 +19,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"Categories";
+    
+    [Moltin sharedInstance].publicId = @"umRG34nxZVGIuCSPfYf8biBSvtABgTR8GMUtflyE";
+    
+    [[Moltin sharedInstance].category listingWithParameters:@{@"status": @1} callback:^(NSDictionary *response, NSError *error) {
+        if (error) {
+            NSLog(@"Category listing ERROR!!! %@", error);
+        }
+        else {
+            NSLog(@"RESPONSE (category listing): %@", response);
+            self.categories = [response objectForKey:@"result"];
+            [self.tableView reloadData];
+        }
+    }];
+    
+    /*[[Moltin sharedInstance].product searchWithParameters:nil callback:^(NSDictionary *response, NSError *error) {
+        if (error) {
+            NSLog(@"ERROR!!! %@", error);
+        }
+        else {
+            NSLog(@"RESPONSE: %@", response);
+        }
+    }];*/
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - TableView delegate
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.categories.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.adjustsFontSizeToFitWidth = YES;
+    
+    NSDictionary *category = [self.categories objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = [category valueForKey:@"title"];
+    
+    return cell;
 }
 
 @end
