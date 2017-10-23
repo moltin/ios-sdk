9595d869534f4f7ae2e69ec6647cc4c34f93e57c
Matthew Foyle <matt@moltin.com>
2017-10-23 08:16:26 +0100

    amend data structure for order to reflect v2 shipping statuses

--- Moltin/Classes/Data Objects/Order.swift
+++ Moltin/Classes/Data Objects/Order.swift
@@ -16,20 +16,19 @@ public struct Order {
         case cancelled
         case complete
     }
-    
+
     public enum Payment: String {
         case unpaid
         case authorized
         case notApplicable = "not_applicable"
         case paid
     }
-    
+
     public enum Shipping: String {
-        case notShipped = "not_shipped"
-        case partial
-        case shipped
+        case unfulfilled
+        case fulfilled
     }
-    
+
     public let id: String
     public let status: Order.Status
     public let payment: Order.Payment
@@ -62,7 +61,7 @@ extension Order: JSONAPIDecodable {
             let billingAddress = Address(json: billingJSON, includedJSON: nil) else {
                 return nil
         }
-        
+
         self.id = id
         self.status = status
         self.payment = payment
@@ -70,28 +69,28 @@ extension Order: JSONAPIDecodable {
         self.customer = customer
         self.shippingAddress = shippingAddress
         self.billingAddress = billingAddress
-        
+
         self.totalProductCount = "meta.counts.products.total" <~~ json
         self.uniqueProductCount = "meta.counts.products.unique" <~~ json
-        
+
         if let valueJSON: JSON = "meta.display_price.with_tax" <~~ json {
             self.displayPriceWithTax = DisplayPrice(json: valueJSON, includedJSON: nil)
         } else {
             self.displayPriceWithTax = nil
         }
-        
+
         if let valueJSON: JSON = "meta.display_price.without_tax" <~~ json {
             self.displayPriceWithoutTax = DisplayPrice(json: valueJSON, includedJSON: nil)
         } else {
             self.displayPriceWithoutTax = nil
         }
-        
+
         if let gatewaysJSON: [JSON] = "meta.gateways" <~~ json {
             self.gateways = [PaymentGateway].from(jsonArray: gatewaysJSON, includedJSON: nil)
         } else {
             self.gateways = []
         }
-        
+
         self.json = json
     }
 }
