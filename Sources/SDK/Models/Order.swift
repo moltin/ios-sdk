67658a9369b3bb4a82a86d39232bc7e78b0a5f51
Craig Tweedy <craig.tweedy@moltin.com>
2018-03-20 15:13:17 +0000

    Added order model

--- Sources/SDK/Models/Order.swift
+++ Sources/SDK/Models/Order.swift
@@ -7,10 +7,26 @@
 
 import Foundation
 
+public class OrderMeta: Codable {
+    public let displayPrice: DisplayPrices
+    public let timestamps: Timestamps
+}
+
+public class OrderRelationships: Codable {
+    public let items: [String: [[String: String]]]
+    public let customer: [String: [String: String]]
+}
+
 public class Order: Codable {
-    var id: String
-    
-    internal init(withID id: String) {
-        self.id = id
-    }
+    public let id: String
+    public let type: String
+    public let status: String
+    public let payment: String
+    public let shipping: String
+    public let customer: Customer
+    public let shippingAddress: Address
+    public let billingAddress: Address
+    public let links: [String: String]
+    public let meta: OrderMeta
+    public let relationships: OrderRelationships
 }
