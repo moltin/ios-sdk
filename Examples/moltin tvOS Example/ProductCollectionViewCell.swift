9b2e92b8e22e99116e5e5180d429b98cebeaa14e
Craig Tweedy <craig.tweedy@moltin.com>
2018-05-23 22:52:52 +0100

    Added collection view for categories and corresponding products view

--- Examples/moltin tvOS Example/ProductCollectionViewCell.swift
+++ Examples/moltin tvOS Example/ProductCollectionViewCell.swift
@@ -2,13 +2,13 @@
 //  ProductCollectionViewCell.swift
 //  moltin tvOS Example
 //
-//  Created by Craig Tweedy on 21/03/2018.
+//  Created by Craig Tweedy on 23/05/2018.
 //
 
 import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
 
-    @IBOutlet var label: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
 
 }
